a80723eb1a83cfd801d87f331b967a1fc566df15
Subham <subhammahanty235@gmail.com>
2025-04-04 17:23:27 +0530

    created the notice

--- pkg/storage/redis.go
+++ pkg/storage/redis.go
@@ -34,7 +34,7 @@ func NewRedisClient(redisUrl string) (*RedisClient, error) {
 	}
 
 	fmt.Println("🔌 Connected to Redis Database")
-
+	PrintWelcomeMessage()
 	return &RedisClient{
 		client: client,
 	}, nil
@@ -46,4 +46,28 @@ func (r *RedisClient) GetRedisClient() *redis.Client {
 
 func (r *RedisClient) CloseRedisClient() error {
 	return r.client.Close()
+}
+
+func PrintWelcomeMessage() {
+	fmt.Println("╔════════════════════════════════════════════════════════════╗")
+	fmt.Println("║                                                            ║")
+	fmt.Println("║   🚀 BizzMQ Queue System v1.0.0 (Early Release)            ║")
+	fmt.Println("║         A lightweight message queue for go                 ║")
+	fmt.Println("║                                                            ║")
+	fmt.Println("╠════════════════════════════════════════════════════════════╣")
+	fmt.Println("║                                                            ║")
+	fmt.Println("║   ⚠️  EARLY VERSION NOTICE                                  ║")
+	fmt.Println("║       This is an early version and may contain bugs.       ║")
+	fmt.Println("║       Please report any issues you encounter to:           ║")
+	fmt.Println("║       github.com/subhammahanty235/bizzmq-go/issues         ║")
+	fmt.Println("║                                                            ║")
+	fmt.Println("║   💡 USAGE                                                 ║")
+	fmt.Println("║       1. Create queues                                     ║")
+	fmt.Println("║       2. Publish messages                                  ║")
+	fmt.Println("║       3. Set up consumers                                  ║")
+	fmt.Println("║                                                            ║")
+	fmt.Println("║   📚 For documentation visit:                              ║")
+	fmt.Println("║       github.com/subhammahanty235/bizzmq-go                ║")
+	fmt.Println("║                                                            ║")
+	fmt.Println("╚════════════════════════════════════════════════════════════╝")
 }
