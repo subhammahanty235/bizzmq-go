6d5ff1c5078c650135b6b001244fde71eb035d08
Subham <subhammahanty235@gmail.com>
2025-06-09 01:41:12 +0530

    basic setup of tcp

--- bizzmq.go
+++ bizzmq.go
@@ -2,14 +2,60 @@
 package bizzmq
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/subhammahanty235/bizzmq-go/internal"
+	pb "github.com/subhammahanty235/bizzmq-go/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
+
+type Server struct {
+	pb.UnimplementedHandShakeServer
+	pb.UnimplementedHeartBeatServer
+}
 
 type BizzMQ struct {
 	redis *internal.RedisClient
+	stop  chan struct{}
+}
+
+func (b *BizzMQ) StartTicker() {
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			CreateTCPServer()
+		case <-b.stop:
+			fmt.Println("Stopping ticker")
+			return
+		}
+	}
+}
+
+func CreateTCPServer() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	fmt.Println("TCP Connection is Running and Ready to process")
+
+	defer conn.Close()
+	client := pb.NewHeartBeatClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	resp, err := client.HeartBeatExchange(ctx, &pb.RequestHeartBeat{InstanceId: "82828", SignalId: "00001"})
+	if err != nil {
+		log.Fatalf("could not exchange heartbeat: %v", err)
+	}
+	log.Printf(resp.InstanceId)
 }
 
 func NewBizzMQ(redisUri string) (*BizzMQ, error) {
@@ -22,9 +68,20 @@ func NewBizzMQ(redisUri string) (*BizzMQ, error) {
 		return nil, fmt.Errorf("failed to connect to Redis: %w", err)
 	}
 
-	return &BizzMQ{
+	b := &BizzMQ{
 		redis: redisInstance,
-	}, nil
+		stop:  make(chan struct{}),
+	}
+	flag := true
+	if flag {
+		go b.StartTicker()
+	}
+
+	return b, err
+
+	// return &BizzMQ{
+	// 	redis: redisInstance,
+	// }, nil
 }
 
 func (b *BizzMQ) Close() error {
